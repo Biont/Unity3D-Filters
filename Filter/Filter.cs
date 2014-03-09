b7d7b8d0679a04bf0475f80d66271798b9e090ad
unknown <arbelzapf@gmail.com>
2014-03-09 12:28:52 +0100

    Initial commit
    Signed-off-by: unknown <arbelzapf@gmail.com>

---
+++ Filter/Filter.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BiontTools
+{
+
+		public static class Filters
+		{
+
+				private static Dictionary<string,Dictionary<string,Func<object,object>>> filters = new Dictionary<string,Dictionary<string,Func<object,object>>> ();
+
+				public static void Add (string filterName, Func<object,object> action, string filterTag = "")
+				{
+
+						if (filterTag == "") {
+								filterTag = Guid.NewGuid ().ToString ();
+						}
+			
+
+						Dictionary<string,Func<object,object>> actionList = null;
+						if (filters.TryGetValue (filterName, out actionList)) {
+								actionList.Add (filterTag, action);
+						} else {
+								filters.Add (filterName, new Dictionary<string,Func<object,object>> (){{filterTag,action}});
+						}
+
+	
+				}
+
+				public static void Remove (string filterName, string filterTag)
+				{
+						Dictionary<string,Func<object,object>> actionList = null;
+						if (filters.TryGetValue (filterName, out actionList)) {
+
+								if (actionList.Remove (filterTag)) {
+										Debug.Log (filterTag + " removed successfully from Filter " + filterName);
+								} else {
+										Debug.Log (filterTag + " not found in " + filterName);
+					
+								}
+								
+						} else {
+								Debug.Log ("Filter " + filterName + " could not be foundr ");
+				
+						}
+				}
+
+	
+				public static object Apply (string filterName, object source)
+				{
+
+						Dictionary<string,Func<object,object>> actionList = null;
+						if (filters.TryGetValue (filterName, out actionList)) {
+
+								foreach (KeyValuePair<string, Func<object,object> > action in actionList) {
+//								foreach (Func<object,object> action in actionList) {
+
+										source = action.Value (source);
+								}
+
+						} else {
+								Debug.Log ("There were no filters added for " + filterName);
+						}
+
+						return source;
+				}
+		}
+
+}
---
+++ FilterBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using BiontTools;
+
+public class FilterBehaviour : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+
+
+				Filters.Add ("test-string", (filterString) => {
+						return "Das geht!";
+				}, "string-override");
+
+
+				string testString = "Was geht?";
+
+				Filters.Remove ("test-string", "string-override");
+
+				testString = (string)Filters.Apply ("test-string", testString);
+				Debug.Log (testString);
+
+
+				Filters.Add ("test-vector3", (filterVector3) => {
+						Vector3 tempVec = (Vector3)filterVector3;
+
+						return new Vector3 (1, 1, 1);
+				});
+		
+		
+				Vector3 testVec = Vector3.zero;
+		
+				testVec = (Vector3)Filters.Apply ("test-vector3", testVec);
+				Debug.Log (testVec);
+
+
+
+
+		}
+	
+
+}
